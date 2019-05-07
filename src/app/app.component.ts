6861231f054ed3c474bc6e1e2aa2545241a8b699
mayur1702 <m.mistry17@yahoo.com>
2019-05-07 09:02:58 +0530

    added awards section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Resume';
+  x= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      console.log(`x is executed`);
+      resolve('x is done')
+    },4000)
+  });
+  y= new Promise((resolve,reject)=>{
+    resolve('y is done')
+  });
+
+
+  ngOnInit(){
+    this.fu();
+    console.log('yes');
+  }
+  async fu(){     
+  const t = await this.x;
+  const o = await this.y;
+  console.log(t,o);
+
+  }
+
 }
+
